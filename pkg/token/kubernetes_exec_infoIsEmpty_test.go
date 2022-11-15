49cd82256f31d1cf5ff0fbe08c6d1bc2a0c5f260
Jing Gao <jing@JingMS>
2022-11-15 16:36:38 -0700

    Add condition:"kubernetes_exec_info" is empty in New() and its unit test.

---
+++ pkg/token/kubernetes_exec_infoIsEmpty_test.go
@@ -0,0 +1,36 @@
+package token
+
+import (
+	"os"
+	"testing"
+)
+
+func TestKUBERNETES_EXEC_INFOIsempty(t *testing.T) {
+	testData := []struct {
+		name            string
+		execInfoEnvTest string
+		options         Options
+	}{
+		{
+			name:            "KUBERNETES_EXEC_INFO is empty",
+			execInfoEnvTest: "",
+			options: Options{
+				LoginMethod: DeviceCodeLogin,
+				ClientID:    "clientID",
+				ServerID:    "serverID",
+				TenantID:    "tenantID",
+			},
+		},
+	}
+
+	for _, data := range testData {
+		t.Run(data.name, func(t *testing.T) {
+			os.Setenv("KUBERNETES_EXEC_INFO", data.execInfoEnvTest)
+			defer os.Unsetenv("KUBERNETES_EXEC_INFO")
+			ecp, err := New(&data.options)
+			if ecp == nil || err != nil {
+				t.Fatalf("expected: return execCredentialPlugin and nil error, actual: did not return execCredentialPlugin or did not return expected error")
+			}
+		})
+	}
+}
