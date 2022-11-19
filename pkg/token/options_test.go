5c35d87b18d2d3a7239478931c3cbb7a9286f165
Weinong Wang <weinongw@microsoft.com>
2022-11-18 17:05:26 -0800

    gofmt -s -w .

--- pkg/token/options_test.go
+++ pkg/token/options_test.go
@@ -23,14 +23,14 @@ func TestOptions(t *testing.T) {
 
 	t.Run("option with customized token cache dir should produce token cache file under specified token cache directory", func(t *testing.T) {
 		o := NewOptions()
-    o.TokenCacheDir = "/tmp/foo/"
+		o.TokenCacheDir = "/tmp/foo/"
 		o.AddFlags(&pflag.FlagSet{})
 		o.UpdateFromEnv()
 		if err := o.Validate(); err != nil {
 			t.Fatalf("option validation failed: %s", err)
 		}
 		dir, _ := filepath.Split(o.tokenCacheFile)
-		if dir != o.TokenCacheDir{
+		if dir != o.TokenCacheDir {
 			t.Fatalf("token cache directory is expected to be %s, got %s", o.TokenCacheDir, dir)
 		}
 	})
