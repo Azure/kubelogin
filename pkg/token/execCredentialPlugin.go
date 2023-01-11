d286547e1aa5dd9c8114ab0c9d2ee10bec314fb7
Tatsinnit <tatsatmishra@gmail.com>
2023-01-11 23:01:17 +0530

    Enable CodeQL Analysis. (#179)
    * Enable CodeQL Analysis.
    * Marshal and remove credentials before logging.
    
    Co-authored-by: Tatsinnit <Tatsinnit@users.noreply.github.com>

--- pkg/token/execCredentialPlugin.go
+++ pkg/token/execCredentialPlugin.go
@@ -29,7 +29,10 @@ type execCredentialPlugin struct {
 }
 
 func New(o *Options) (ExecCredentialPlugin, error) {
-	klog.V(10).Info(o)
+
+	logginOptionsObject := marshalOptionsForLogging(o)
+
+	klog.V(10).Info(logginOptionsObject)
 	provider, err := newTokenProvider(o)
 	if err != nil {
 		return nil, err
@@ -46,6 +49,26 @@ func New(o *Options) (ExecCredentialPlugin, error) {
 		refresher:            newManualToken,
 		disableTokenCache:    disableTokenCache,
 	}, nil
+}
+
+func marshalOptionsForLogging(o *Options) KlogsLoggingPurposeOptions {
+	logginOptionsObject := KlogsLoggingPurposeOptions{
+		LoginMethod:            o.LoginMethod,
+		ClientID:               o.ClientID,
+		ClientCert:             o.ClientCert,
+		Username:               o.Username,
+		ServerID:               o.ServerID,
+		TenantID:               o.TenantID,
+		Environment:            o.Environment,
+		IsLegacy:               o.IsLegacy,
+		TokenCacheDir:          o.TokenCacheDir,
+		tokenCacheFile:         o.tokenCacheFile,
+		IdentityResourceID:     o.IdentityResourceID,
+		FederatedTokenFile:     o.FederatedTokenFile,
+		AuthorityHost:          o.AuthorityHost,
+		UseAzureRMTerraformEnv: o.UseAzureRMTerraformEnv,
+	}
+	return logginOptionsObject
 }
 
 func (p *execCredentialPlugin) Do() error {
