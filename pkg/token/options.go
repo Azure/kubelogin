eef15ac9b7fe33300f9f64d2452eb26b8da4c7b5
hbc <bcxxxxxx@gmail.com>
2023-12-18 14:46:00 -0800

    feat: define types

---
+++ pkg/token/options.go
@@ -0,0 +1,41 @@
+package token
+
+import "github.com/Azure/kubelogin/pkg/internal/token"
+
+// list of supported login methods for library consumers
+
+const (
+	ServicePrincipalLogin = token.ServicePrincipalLogin
+	ROPCLogin             = token.ROPCLogin
+	MSILogin              = token.MSILogin
+	WorkloadIdentityLogin = token.WorkloadIdentityLogin
+)
+
+// Options defines the options for getting token.
+type Options struct {
+	LoginMethod string
+
+	// shared login settings
+
+	Environment string
+	TenantID    string
+	ServerID    string
+	ClientID    string
+
+	// for ServicePrincipalLogin & ROPCLogin
+
+	ClientSecret       string
+	ClientCert         string
+	ClientCertPassword string
+	IsPopTokenEnabled  bool
+	PoPTokenClaims     string
+
+	// for MSILogin
+
+	IdentityResourceID string
+
+	// for WorkloadIdentityLogin
+
+	AuthorityHost      string
+	FederatedTokenFile string
+}
